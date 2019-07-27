0fd8495af874b921f9e404987abd84cf4ed3967f
darms <darmcca@gmail.com>
2019-07-26 21:54:15 -0700

    basic structure

---
+++ moviecyclr-app/src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+
+class Home extends Component {
+    state = {
+
+        render() {
+            return(
+                <div>
+                    Home
+                </div>
+            )
+        }
+
+    }
+
+
+}
+
+export default Home;
